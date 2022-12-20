bc4cb51d822f34cef7529e5c6a04e6aa04cedc1a
Nuth Vireak <timberr.4t@gmail.com>
2022-12-21 01:51:36 +0700

    // TO-DO: 3:30:00 Unit Testing Service Layer

--- src/main/java/com/dailycodebuffer/sprinboot/tutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/dailycodebuffer/sprinboot/tutorial/SpringBootTutorialApplication.java
@@ -10,7 +10,6 @@ public class SpringBootTutorialApplication {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
 	}
 
-    // TO-DO: 3:26:00 Changing H2 -> MYSQL
-	// Today
+    // TO-DO: 3:30:00 Unit Testing Service Layer
 }
 
