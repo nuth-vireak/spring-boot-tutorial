adc49fc20d2e5726715cf438ba5d8ea11a8c3969
nuth-vireak <timberr.4t@gmail.com>
2023-01-27 17:13:27 +0700

    // TODO: Adding Config in properties file 4:00:00

--- src/main/java/com/dailycodebuffer/sprinboot/tutorial/controller/HelloController.java
+++ src/main/java/com/dailycodebuffer/sprinboot/tutorial/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.dailycodebuffer.sprinboot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String hellWorld() {
-        return "Hello World";
+        return welcomeMessage;
     }
 }
